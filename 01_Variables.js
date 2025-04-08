17bafb3dd7286d3685efc0d5f7db45dea1676dd7
Giri047 <102897495+Giri047@users.noreply.github.com>
2025-04-08 20:17:07 +0000

    New file added for variables and constants.

---
+++ 01_Variables.js
@@ -0,0 +1,15 @@
+const accountId1=203402
+let accountEmail="sushobhansaha@google.com"
+var accountPassword='peregrine'
+accountCity='Gurugram'
+
+console.table([accountId1,accountEmail,accountPassword,accountCity]);
+
+// accountId =3 is not allowed as it is constant and is immutable
+
+const accountId2 = 303000
+accountEmail="shrutirawat@google.com"
+var accountPassword='falcon'
+accountCity='Bengaluru'
+
+console.table([accountId2,accountEmail,accountPassword,accountCity])
