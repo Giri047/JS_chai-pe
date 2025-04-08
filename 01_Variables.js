19911fe2cc633d4e943e54381fe63a2d3491dc71
Giri047 <102897495+Giri047@users.noreply.github.com>
2025-04-08 20:25:06 +0000

    new update

--- 01_Variables.js
+++ 01_Variables.js
@@ -1,15 +1,19 @@
 const accountId1=203402
 let accountEmail="sushobhansaha@google.com"
 var accountPassword='peregrine'
-accountCity='Gurugram'
+let accountCity='Gurugram'
+accountState="Haryana"; // not using let also can be a way to define a variable
 
-console.table([accountId1,accountEmail,accountPassword,accountCity]);
+console.table([accountId1,accountEmail,accountPassword,accountCity,accountState]);
 
 // accountId =3 is not allowed as it is constant and is immutable
 
 const accountId2 = 303000
 accountEmail="shrutirawat@google.com"
-var accountPassword='falcon'
+accountPassword='falcon'
 accountCity='Bengaluru'
+let accountState2;
 
-console.table([accountId2,accountEmail,accountPassword,accountCity])
+console.table([accountId2,accountEmail,accountPassword,accountCity,accountState2])
+/*
+NOTE : Prefer not to use var because of issue in block scope and functional scope*/
